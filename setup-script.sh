1fd1da1b2f5ab9126bcd5d90a2de7a4d45f0b422
Imthath <imthath.m@hotmail.com>
2024-03-02 19:02:55 +0530

    Set paths in script and add more brew installs

--- setup-script.sh
+++ setup-script.sh
@@ -5,6 +5,12 @@ killall Dock
 
 #install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Add Homebrew to your PATH in ~/.zprofile:
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # install apps and command line tools via brew
 brew bundle install
+
+# set path for Java
+export PATH="/usr/local/opt/openjdk/bin:$PATH"
