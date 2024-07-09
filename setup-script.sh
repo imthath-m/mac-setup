c46eac28d8c255738e807c289e4c9dacf710cb32
Mohammed Imthathullah <imthath.m@hotmail.com>
2024-07-09 13:19:35 +0530

    Reduce default casks and formulae. Check for Xcode Command Line tools installation at start

--- setup-script.sh
+++ setup-script.sh
@@ -3,6 +3,26 @@ defaults write com.apple.dock persistent-apps -array
 # Restart dock for changes to take effect
 killall Dock
 
+xcode-select --install
+
+expected="cltools-installed"
+echo "Please follow the instrcutions in the Xcode Command Line Tools installation dialog box"
+echo "Come back here once you are done with it"
+echo "Enter $expected to proceed"
+read status
+
+if [ $status != $expected ]
+then 
+    echo "Exiting script. Please install Xcode Command Line Tools by running following command and then proceed"
+    echo "xcode-select --install"
+    echo "If you have already installed then properly, please type correctly $expected when prompted"
+    exit 1
+fi
+
+echo "Xcode Command Line Tools installed. Proceeding to install Homebrew."
+
+# write code to wait for dialog to complete installation
+
 #install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # Add Homebrew to your PATH in ~/.zprofile:
